e4a0399737406a07e08f89838024840b008495ae
Eric Borts <ericbort@amazon.com>
2021-06-24 15:22:07 -0700

    Python "standardization" recommendations - PEP8 formatting (warnings in pycharm) - wrap main code in main(), protect file with standard if __name__ == "__main__" - use f-string formatting (modern best practice, ease of use) - underscore-separate names for readability (standardization, and solves warnings in spell checker) - remove unncessary parens :D - added requirements.txt, so other users can get up and started easily with `pip install -r requirements.txt` - add .gitignore file (IDE and venv ignores)

--- radlog.py
+++ radlog.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-
-import serial
-import rrdtool
 import os.path
 
-def createrrd():
+import rrdtool
+import serial
+
+
+def create_rrd():
     # 600 samples of 5 minutes  (2 days and 2 hours)
     # 700 samples of 30 minutes (2 days and 2 hours, plus 12.5 days)
     # 775 samples of 2 hours    (above + 50 days)
@@ -12,37 +13,44 @@ def createrrd():
     if os.path.exists("rads.rrd"):
         print("Using existing RRD...")
     else:
-        rrdtool.create("rads.rrd",
-                "--start", "now",
-                "--step", "300",
-                "DS:cps:GAUGE:600:U:U",
-                "DS:cpm:GAUGE:600:U:U",
-                "RRA:AVERAGE:0.5:1:600",
-                "RRA:AVERAGE:0.5:6:700",
-                "RRA:AVERAGE:0.5:24:775",
-                "RRA:AVERAGE:0.5:288:797",
-                "RRA:MAX:0.5:1:600",
-                "RRA:MAX:0.5:6:700",
-                "RRA:MAX:0.5:24:775",
-                "RRA:MAX:0.5:288:797")
+        rrdtool.create(
+            "rads.rrd",
+            "--start", "now",
+            "--step", "300",
+            "DS:cps:GAUGE:600:U:U",
+            "DS:cpm:GAUGE:600:U:U",
+            "RRA:AVERAGE:0.5:1:600",
+            "RRA:AVERAGE:0.5:6:700",
+            "RRA:AVERAGE:0.5:24:775",
+            "RRA:AVERAGE:0.5:288:797",
+            "RRA:MAX:0.5:1:600",
+            "RRA:MAX:0.5:6:700",
+            "RRA:MAX:0.5:24:775",
+            "RRA:MAX:0.5:288:797")
 
-ser = serial.Serial('/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_AB0KPLG7-if00-port0',baudrate=9600, bytesize=8,parity='N',stopbits=1, timeout=3, xonxoff=0, rtscts=0)
 
-print(ser.baudrate)
-print(ser.name)
+def main():
+    ser = serial.Serial('/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_AB0KPLG7-if00-port0',
+                        baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=3, xonxoff=0, rtscts=0)
 
-createrrd()
+    print(ser.baudrate)
+    print(ser.name)
 
-while (1):
-    # b'CPS, 0, CPM, 13, uSv/hr, 0.07, SLOW\r\n'
-    line = ser.readline()
-    string = line.decode()
-    chunks = string.split(',')
-    if (len(chunks) == 7):
-        cps = chunks[1]
-        cpm = chunks[3]
-        print("Logging N:"+cps+":"+cpm)
-        rrdtool.update("rads.rrd","N:"+cps+":"+cpm)
-    else:
-        print("Bad read:", line)
+    create_rrd()
 
+    while True:
+        # b'CPS, 0, CPM, 13, uSv/hr, 0.07, SLOW\r\n'
+        line = ser.readline()
+        string = line.decode()
+        chunks = string.split(',')
+        if len(chunks) == 7:
+            cps = chunks[1]
+            cpm = chunks[3]
+            print(f"Logging N:{cps}:{cpm}")
+            rrdtool.update("rads.rrd", f"N:{cps}:{cpm}")
+        else:
+            print(f"Bad read: {line}")
+
+
+if __name__ == "__main__":
+    main()
